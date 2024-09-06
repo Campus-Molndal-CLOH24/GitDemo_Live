a4ae009a8fa290d17966eb3f7ba7b8dce80389c7
Marcus <coder@marcusmedina.pro>
2024-09-06 11:20:32 +0200

    10 rader

--- GitDemo_Live/Program.cs
+++ GitDemo_Live/Program.cs
@@ -5,5 +5,9 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, Test!");
+        for (int rows = 0; rows < 10; rows++)
+        {
+            Console.WriteLine("Row:" + rows);
+        }
     }
 }
